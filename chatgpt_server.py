f263f4464eea6c3df691dad0d405995300409d4d
Chen Wu <201436728@qq.com>
2023-01-31 17:00:59 +0800

    Update chatgpt_server.py

--- chatgpt_server.py
+++ chatgpt_server.py
@@ -16,12 +16,12 @@ isBusy = False
 def chat():
     global isBusy
     if isBusy:
-        return jsonify({"code": 429, "msg": "ChatGPT正忙"})
+        return jsonify({"code": 429, "msg": "ChatGPT姝ｅ繖"})
     try:
         isBusy = True
         prompt = request.form.get("prompt")
         data = chatbot.get_chat_response(prompt)
-        return jsonify({"code": 0, "msg": "成功", "data": data})
+        return jsonify({"code": 0, "msg": "鎴愬姛", "data": data})
     except Exception as e:
         print(e)
         return jsonify({"code": 500, "msg": str(e)})
@@ -31,4 +31,4 @@ def chat():
 @app.route("/reset", methods=["GET"])
 def reset():
     chatbot.reset_chat()
-    return jsonify({"code": 0, "msg": "成功"})
+    return jsonify({"code": 0, "msg": "鎴愬姛"})
